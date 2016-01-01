a326687b16b154d4cc9887b7d8b1d1e765d2d3b7
JP DeVries <mail@devries.jp>
2016-01-01 13:51:19 -0800

    New Configurations
    Oscillation Range
    Oscillation Frequency
    Fill Polygons
    Draw Non-Polygon Shapes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,43 @@
 window.onload = function() {
+
+  var distortShapes = false;
+  var polygonData = [];
+  var lastMousePos = new Point(window.innerWidth * .5, window.innerHeight * .5); // assume the mouse starts at the center
+
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var osscillate = document.querySelector('input[name="osscillate"]');
+  var fillPolygons = document.querySelector('input[name="fill_polygons"]');
+  var oscillationRange = document.querySelector('input[name="osc_range"]');
+  var oscillationFrequency = document.querySelector('input[name="osc_freq"]');
+  var drawNonPoly = document.querySelector('input[name="draw_non_poly"]');
+  var compmode = document.getElementById('compmode');
+  var distortCheckbox = document.querySelector('input[name="distort_shapes"]');
+
   function Point(x, y) {
     this.x = x;
     this.y = y;
   }
 
-  function Polygon(fill,points,tick, freq,range) {
+  function Polygon(fill,points,tick) {
     tick = typeof tick !== 'undefined' ?  b : 0;
-    freq = typeof freq !== 'undefined' ?  freq : 0.01;
-    range = typeof range !== 'undefined' ? range : 8;
     return {
       points:points,
       fill:fill,
       tick:tick,
-      freq:freq,
-      range:range,
       step:function() {
-        this.tick += this.freq;
+        this.tick += parseFloat(oscillationFrequency.value);
       }
     }
   }
-
-  var distortShapes = false;
-  var lastMousePos = new Point(window.innerWidth * .5, window.innerHeight * .5); // assume the mouse starts at the center
-  var canvas = document.getElementById("canvas");
-  var canvas_bg = document.getElementById("canvas_bg");
-  var draw_bg = document.querySelector('input[name="draw_bg"]');
-  var ctx = canvas.getContext("2d");
-  var osscillate =document.querySelector('input[name="osscillate"]');
-  var polygonData = [];
 
   document.body.onmousemove = function(e) {
     lastMousePos = new Point(e.pageX, e.pageY); // current mouse position
   };
 
   function handleResize() {
-    canvas.width = canvas_bg.width = window.innerWidth;
-    canvas.height = canvas_bg.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
   }
 
   window.onresize = function() {
@@ -44,7 +46,7 @@ window.onload = function() {
 
   handleResize();
 
-  (function(){
+  (function(){ // for each <polygon> in the source SVG extract points,fill and remove
     var polygons = document.querySelectorAll('#wolf polygon');
 
     for(var i = 0; i < polygons.length; i++) {
@@ -60,7 +62,7 @@ window.onload = function() {
   })();
 
   var source = new Image();
-  (function(){ // load the remaining SVG image onto the canvas as an unmanipulatable Image
+  (function(source){ // create an image of the remaining non-polygonal shapes
     var DOMURL = window.URL || window.webkitURL || window;
 
     var data = document.querySelector("#wolf").outerHTML;
@@ -82,13 +84,9 @@ window.onload = function() {
     });
 
     source.src = url;
-  })();
+  })(source);
 
-  var compmode = document.getElementById('compmode');
-  var distortCheckbox = document.querySelector('input[name="distort_shapes"]');
-  function step(timestamp) {
-    draw_bg.disabled = !distortCheckbox.checked;
-
+  function step(timestamp) { // for each step through the animation
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.globalCompositeOperation = compmode.value;
     for(var i = 0; i < polygonData.length; i++) {
@@ -104,7 +102,7 @@ window.onload = function() {
     }
 
     window.requestAnimationFrame(step);
-    if(!distortCheckbox.checked) ctx.drawImage(source, (window.innerWidth / 2 )- (522/2), (window.innerHeight / 2 )- (620/2));
+    if(drawNonPoly.checked) ctx.drawImage(source, (window.innerWidth / 2 )- (522/2), (window.innerHeight / 2 )- (620/2));
   }
 
   window.requestAnimationFrame(step);
@@ -115,36 +113,11 @@ window.onload = function() {
     var fillStyle = polygon.fill;
     ctx.strokeStyle = strokeStyle;
 
-    for(var i = 0; i < points.length; i++) {
-      var point = points[i];
-      var x = parseFloat(point.split(',')[0]);
-      var y = parseFloat(point.split(',')[1]);
-
-      var closestDist;
-      var p = new Point(x,y);
-      var d = distanceBetweenTwoPoints(lastMousePos,p);
-      if(closestDist === undefined || d < closestDist) {
-        closestDist = d;
-      }
-
-      function distanceBetweenTwoPoints(_a,_b) {
-        return Math.sqrt(Math.pow(Math.abs(_a.y - _b.y),2) + Math.pow(Math.abs(_a.x - _b.x),2))
-      }
-    }
-
-    if(draw_bg.checked) canvas_bg.classList.remove('hidden');
-    else canvas_bg.classList.add('hidden');
-
     if(distortCheckbox.checked) {
       points[0] = (lastMousePos.x) + ',' + (lastMousePos.y);
-      canvas_bg.classList.remove('hidden');
     } else {
-      var destCtx = canvas_bg.getContext('2d');
-      if(draw_bg.checked) destCtx.drawImage(canvas,0,0);
-      canvas_bg.classList.add('hidden');
+
     }
-
-
 
     for(var i = 0; i < points.length; i++) {
       var point = points[i];
@@ -153,12 +126,11 @@ window.onload = function() {
 
       if(osscillate.checked) {
         if(i == 1) {
-          x += Math.sin(polygon.tick) * polygon.range;
+          x += Math.sin(polygon.tick) * oscillationRange.value;
         } else if(i == 2) {
-          y += Math.sin(polygon.tick) * polygon.range;
+          y += Math.sin(polygon.tick) * oscillationRange.value;
         }
       }
-
 
       if(i !== 0 || !distortCheckbox.checked) {
         x += (window.innerWidth - 522) / 2;
@@ -174,7 +146,15 @@ window.onload = function() {
 
     ctx.closePath();
     ctx.fillStyle = fillStyle;
-    ctx.stroke();
-    ctx.fill();
+
+    if(fillPolygons.checked) {
+      ctx.fill();
+    } else {
+      ctx.stroke();
+    }
+
+    function distanceBetweenTwoPoints(_a,_b) {
+      return Math.sqrt(Math.pow(Math.abs(_a.y - _b.y),2) + Math.pow(Math.abs(_a.x - _b.x),2))
+    }
   }
 }
