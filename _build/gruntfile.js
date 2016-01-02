aa38204e86cfd7fa45e94905b7921b31f1bbe049
JP DeVries <mail@devries.jp>
2016-01-02 03:42:25 -0800

    Grunt workflow and comments

--- _build/gruntfile.js
+++ _build/gruntfile.js
@@ -1,102 +1,47 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     dirs:{
-        'lib':'./lib/',
+        lib:'./lib/',
         theme: '../',
         assets:'assets/',
-        scss:'./scss/',
-        css:'css/',
         js:'js/'
-    },
-    copy: {
-      canvg: {
-        files: [
-          {src:'canvg/**/*',cwd:'<%= dirs.lib %>',dest:'<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>vendor/',expand:true}
-        ]
-      }
-    },
-    sass: {
-        dist: {
-    		options:{
-        		style: 'compressed',
-        		compass: false
-    		},
-    		files: {
-    			'<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.css': '<%= dirs.scss %>main.scss'
-    		}
-        }
-    },
-    autoprefixer: {
-      options: {
-          browsers: ['last 2 versions', 'ie 8', 'ie 9']
-      },
-      dist: {
-  		files: {
-  			'<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.css': '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.css'
-  		}
-      },
-    },
-    cssmin: {
-      options: {
-        shorthandCompacting: false,
-        roundingPrecision: -1
-      },
-      target: {
-        files: {
-          '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.min.css': '<%= dirs.theme %><%= dirs.assets %><%= dirs.css %>main.css'
-        }
-      }
     },
     uglify: {
         js: {
           files: {
-            '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>app.es5.min.js': '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>app.es5.js',
-              '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>app.min.js': '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>app.js',
+            '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>app.min.js': '<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>app.js'
           }
         }
     },
     watch: {
-		options: {
-			livereload: true
-		},
-        scss: {
-            files:['<%= dirs.scss %>*.scss'],
-            tasks:['sass','autoprefixer','growl:scss']
-        },
-        js: {
-            files:['<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>*.js'],
-            tasks:['react','babel','growl:jsx']
-        }
+  		options: {
+  			livereload: true
+  		},
+      js: {
+          files:['<%= dirs.theme %><%= dirs.assets %><%= dirs.js %>*.js','!*.min.js'],
+          tasks:['uglify','growl:uglify']
+      }
     },
 	growl: { /* optional growl notifications requires terminal-notifer: gem install terminal-notifier */
 		build: {
 			title: "grunt",
 			message: "Build complete."
 		},
-		scss: {
-			title: "grunt",
-			message: "Stylesheets created."
-		},
 		watch: {
 			title: "grunt",
 			message: "Watching. Grunt has its eye on you."
 		},
-		jsx: {
+		uglify: {
 			title: "grunt",
-			message: "Compiled JSX files."
+			message: "JavaScript minified."
 		}
 	}
   });
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-
   grunt.loadNpmTasks('grunt-growl');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('default', ['growl:watch', 'watch']);
-  grunt.registerTask('build', ['sass','autoprefixer','cssmin', 'uglify', 'growl:build']);
+  grunt.registerTask('build', ['uglify', 'growl:build']);
 };
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,39 @@
 window.onload = function() {
 
-  var distortShapes = false;
-  var polygonData = [];
-  var lastMousePos = new Point(window.innerWidth * .5, window.innerHeight * .5); // assume the mouse starts at the center
+  var followMouse = false, // whether or not to "follow the mouse"
+  polygonData = [], // array of data objects, one for each point
+  lastMousePos = new Point(window.innerWidth * .5, window.innerHeight * .5), // where the mouse was last, assume the mouse starts at the center
 
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  var osscillate = document.querySelector('input[name="osscillate"]');
-  var fillPolygons = document.querySelector('input[name="fill_polygons"]');
-  var oscillationRange = document.querySelector('input[name="osc_range"]');
-  var oscillationFrequency = document.querySelector('input[name="osc_freq"]');
-  var drawNonPoly = document.querySelector('input[name="draw_non_poly"]');
-  var compmode = document.getElementById('compmode');
-  var distortCheckbox = document.querySelector('input[name="distort_shapes"]');
+  canvas = document.getElementById("canvas"), // our <canvas> tag in the index.html DOM
+  ctx = canvas.getContext("2d"),
+  // query the DOM once for these elements by using pointer variables
+  osscillate = document.querySelector('input[name="osscillate"]'),
+  fillPolygons = document.querySelector('input[name="fill_polygons"]'),
+  oscillationRange = document.querySelector('input[name="osc_range"]'),
+  oscillationFrequency = document.querySelector('input[name="osc_freq"]'),
+  drawNonPoly = document.querySelector('input[name="draw_non_poly"]'),
+  compmode = document.getElementById('compmode'),
+  followMouseCheckbox = document.querySelector('input[name="follow_mouse"]');
 
-  function Point(x, y) {
+  function Point(x, y) { // used to store coordinates 
     this.x = x;
     this.y = y;
   }
 
-  function Polygon(fill,points,tick) {
-    tick = typeof tick !== 'undefined' ?  b : 0;
+  function Polygon(fill,points,tick) { // data object for polygons
+    tick = typeof tick !== 'undefined' ?  tick : 0; // a number incremented each step. the Math.sin() of this value will be used to create our oscillation effect http://codepen.io/jpdevries/pen/BjLOeY
     return {
       points:points,
       fill:fill,
       tick:tick,
-      step:function() {
+      step:function() { // each step increment the tick by the current frequency
         this.tick += parseFloat(oscillationFrequency.value);
       }
     }
   }
 
-  document.body.onmousemove = function(e) {
-    lastMousePos = new Point(e.pageX, e.pageY); // current mouse position
+  document.body.onmousemove = function(e) { // whenever the mouse is moved
+    lastMousePos = new Point(e.pageX, e.pageY); // store the current mouse position
   };
 
   function handleResize() {
@@ -41,28 +42,93 @@ window.onload = function() {
   }
 
   window.onresize = function() {
-    handleResize();
+    handleResize(); // keep the canvas full screen
   };
 
   handleResize();
 
-  (function(){ // for each <polygon> in the source SVG extract points,fill and remove
+  (function(){ // for each <polygon> in the source SVG extract points, fill and then remove
     var polygons = document.querySelectorAll('#wolf polygon');
 
     for(var i = 0; i < polygons.length; i++) {
-      var polygon = polygons[i];
-      var points = polygon.getAttribute('points');
-      var fill = polygon.getAttribute('fill');
+      var polygon = polygons[i],
+      points = polygon.getAttribute('points').trim(),
+      fill = polygon.getAttribute('fill');
 
-      // remove the shape from the SVG
-      polygon.remove();
+      polygon.remove(); // remove the shape from the SVG
 
-      polygonData.push(new Polygon(fill,points));
+      polygonData.push(new Polygon(fill,points)); // create a new Polygon and store it. We'll loop through all these to draw them on each step()
     }
   })();
 
-  var source = new Image();
-  (function(source){ // create an image of the remaining non-polygonal shapes
+  var source = createImageOfNonPolygonalShapes(); // create an image of the remaining non-polygonal shapes
+
+  function step(timestamp) { // for each step through the animation
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
+    ctx.globalCompositeOperation = compmode.value; // update the "blend mode"
+
+    for(var i = 0; i < polygonData.length; i++) { // for each polygon
+      var polygon = polygonData[i];
+
+      drawShape(ctx,polygon); // draw the shape onto the <canvas>
+
+      polygon.step();
+    }
+
+    if(drawNonPoly.checked) ctx.drawImage(source, (window.innerWidth / 2 )- (522/2), (window.innerHeight / 2 )- (620/2)); // draw non-polygonal shapes and center them on <canvas>
+
+    window.requestAnimationFrame(step); // keep the animation running
+  }
+
+  window.requestAnimationFrame(step); // start the animation
+
+  function drawShape(ctx,polygon) { // draw the polygon data to a <canvas> context
+    var points = polygon.points.split(' '),
+    strokeStyle = polygon.fill,
+    fillStyle = polygon.fill;
+
+    if(followMouseCheckbox.checked) { // "follow the mouse" if we should
+      points[0] = (lastMousePos.x) + ',' + (lastMousePos.y);
+    }
+
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillStyle = fillStyle;
+
+    ctx.beginPath();
+
+    for(var i = 0; i < points.length; i++) { // for each point in the polygon
+      var point = points[i],
+      x = parseFloat(point.split(',')[0]),
+      y = parseFloat(point.split(',')[1]);
+
+      if(osscillate.checked) { // apply oscillation effect using a sin wave
+        if(i == 1) {
+          x += Math.sin(polygon.tick) * oscillationRange.value; // second point in polygon moves horizontally
+        } else if(i == 2) {
+          y += Math.sin(polygon.tick) * oscillationRange.value; // third point in polygol moves vertically
+        }
+      }
+
+      if(i !== 0 || !followMouseCheckbox.checked) { // adjust coordinates for stage size
+        x += (window.innerWidth - 522) / 2;
+        y += (window.innerHeight - 620) / 2;
+      }
+
+      if(i<1) {
+        ctx.moveTo(x,y); // move to the position of the first point
+      } else {
+        ctx.lineTo(x,y); // draw lines to the rest
+      }
+    }
+
+    ctx.closePath(); // close the path and set the fill style
+
+    if(fillPolygons.checked) ctx.fill(); // fill the path or
+    else ctx.stroke(); // stroke the path
+  }
+
+  function createImageOfNonPolygonalShapes() { // takes the remaining <svg> data from our #wolf element and prepare it for being drawn to the canvas
+    var source = new Image();
     var DOMURL = window.URL || window.webkitURL || window;
 
     var data = document.querySelector("#wolf").outerHTML;
@@ -84,77 +150,7 @@ window.onload = function() {
     });
 
     source.src = url;
-  })(source);
 
-  function step(timestamp) { // for each step through the animation
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.globalCompositeOperation = compmode.value;
-    for(var i = 0; i < polygonData.length; i++) {
-      var polygon = polygonData[i];
-
-      // draw the shape onto the <canvas>
-      drawShape(ctx,polygon,(function(){
-        var a = polygon.points.split(' ');
-        a.pop();
-        return a;
-      })());
-      polygon.step();
-    }
-
-    window.requestAnimationFrame(step);
-    if(drawNonPoly.checked) ctx.drawImage(source, (window.innerWidth / 2 )- (522/2), (window.innerHeight / 2 )- (620/2));
-  }
-
-  window.requestAnimationFrame(step);
-
-  function drawShape(ctx,polygon,points) {
-    ctx.beginPath();
-    var strokeStyle = polygon.fill;
-    var fillStyle = polygon.fill;
-    ctx.strokeStyle = strokeStyle;
-
-    if(distortCheckbox.checked) {
-      points[0] = (lastMousePos.x) + ',' + (lastMousePos.y);
-    } else {
-
-    }
-
-    for(var i = 0; i < points.length; i++) {
-      var point = points[i];
-      var x = parseFloat(point.split(',')[0]);
-      var y = parseFloat(point.split(',')[1]);
-
-      if(osscillate.checked) {
-        if(i == 1) {
-          x += Math.sin(polygon.tick) * oscillationRange.value;
-        } else if(i == 2) {
-          y += Math.sin(polygon.tick) * oscillationRange.value;
-        }
-      }
-
-      if(i !== 0 || !distortCheckbox.checked) {
-        x += (window.innerWidth - 522) / 2;
-        y += (window.innerHeight - 620) / 2;
-      }
-
-      if(i<1) {
-        ctx.moveTo(x,y);
-      } else {
-        ctx.lineTo(x,y);
-      }
-    }
-
-    ctx.closePath();
-    ctx.fillStyle = fillStyle;
-
-    if(fillPolygons.checked) {
-      ctx.fill();
-    } else {
-      ctx.stroke();
-    }
-
-    function distanceBetweenTwoPoints(_a,_b) {
-      return Math.sqrt(Math.pow(Math.abs(_a.y - _b.y),2) + Math.pow(Math.abs(_a.x - _b.x),2))
-    }
+    return source;
   }
 }
