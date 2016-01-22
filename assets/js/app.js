8aa5006f94d449dea1cc756da61457f42cc98f07
JP DeVries <johnpaul.devries@gmail.com>
2016-01-21 17:34:20 -0800

    Removing Art and Configurations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,88 @@
 window.onload = function() {
-
-  var followMouse = false, // whether or not to "follow the mouse"
-  polygonData = [], // array to hold data objects, one for each point
-  lastMousePos = Point(window.innerWidth * .5, window.innerHeight * .5), // where the mouse was last, assume the mouse starts at the center
-
+  var polygonData = [], // array to hold data objects, one for each point
   canvas = document.getElementById("canvas"), // our <canvas> tag in the index.html DOM
-  ctx = canvas.getContext("2d"),
-  // query the DOM once for these elements by using pointer variables
-  oscillate = document.querySelector('input[name="oscillate"]'),
-  fillPolygons = document.querySelector('input[name="fill_polygons"]'),
-  oscillationRange = document.querySelector('input[name="osc_range"]'),
-  oscillationFrequency = document.querySelector('input[name="osc_freq"]'),
-  drawNonPoly = document.querySelector('input[name="draw_non_poly"]'),
-  compmode = document.getElementById('compmode'),
-  followMouseCheckbox = document.querySelector('input[name="follow_mouse"]');
+  oscillationRange = 8,
+  oscillationFrequency = .01,
+  ctx = canvas.getContext("2d");
 
+  function handleResize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
 
-  checkUrlParams();
+  window.onresize = function() {
+    handleResize(); // keep the canvas full screen
+  };
+  handleResize();
+
+  (function(){ // for each <polygon> in the source SVG extract points, fill and then remove
+    var polygons = document.querySelectorAll('#art polygon');
+
+    for(var i = 0; i < polygons.length; i++) {
+      var polygon = polygons[i],
+      points = polygon.getAttribute('points').trim(),
+      fill = polygon.getAttribute('fill');
+
+      polygon.remove(); // remove the shape from the SVG
+
+      polygonData.push(Polygon(fill,points)); // create a Polygon and store it. We'll loop through all these to draw them on each step()
+    }
+  })();
+
+  function step(timestamp) { // for each step through the animation
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
+    ctx.globalCompositeOperation = 'multiply';
+
+    for(var i = 0; i < polygonData.length; i++) { // for each polygon
+      var polygon = polygonData[i];
+
+      drawShape(ctx,polygon); // draw the shape onto the <canvas>
+
+      polygon.step();
+    }
+
+    window.requestAnimationFrame(step); // keep the animation running
+  }
+
+  window.requestAnimationFrame(step); // start the animation
+
+  function drawShape(ctx,polygon) { // draw the polygon data to a <canvas> context
+    var points = polygon.points.split(' '),
+    strokeStyle = polygon.fill,
+    fillStyle = polygon.fill;
+
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillStyle = fillStyle;
+
+    ctx.beginPath();
+
+    for(var i = 0; i < points.length; i++) { // for each point in the polygon
+      var point = points[i],
+      x = parseFloat(point.split(',')[0]),
+      y = parseFloat(point.split(',')[1]);
+
+      if(i == 1) {
+        x += Math.sin(polygon.tick) * oscillationRange; // second point in polygon moves horizontally
+      } else if(i == 2) {
+        y += Math.sin(polygon.tick) * oscillationRange; // third point in polygol moves vertically
+      }
+
+      // adjust coordinates for stage size
+      x += (window.innerWidth - 522) / 2;
+      y += (window.innerHeight - 620) / 2;
+
+      if(i<1) {
+        ctx.moveTo(x,y); // move to the position of the first point
+      } else {
+        ctx.lineTo(x,y); // draw lines to the rest
+      }
+    }
+
+    ctx.closePath(); // close the path and set the fill style
+
+    ctx.fill(); // fill the path or
+  }
+
 
   function Point(x, y) { // used to store coordinates
     return {
@@ -32,158 +98,8 @@ window.onload = function() {
       fill:fill,
       tick:tick,
       step:function() { // each step increment the tick by the current frequency
-        this.tick += parseFloat(oscillationFrequency.value);
+        this.tick += parseFloat(oscillationFrequency);
       }
     }
   }
-
-  document.body.onmousemove = function(e) { // whenever the mouse is moved
-    lastMousePos = Point(e.pageX, e.pageY); // store the current mouse position
-  };
-
-  function handleResize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
-
-  window.onresize = function() {
-    handleResize(); // keep the canvas full screen
-  };
-
-  handleResize();
-
-  (function(){ // for each <polygon> in the source SVG extract points, fill and then remove
-    var polygons = document.querySelectorAll('#wolf polygon');
-
-    for(var i = 0; i < polygons.length; i++) {
-      var polygon = polygons[i],
-      points = polygon.getAttribute('points').trim(),
-      fill = polygon.getAttribute('fill');
-
-      polygon.remove(); // remove the shape from the SVG
-
-      polygonData.push(Polygon(fill,points)); // create a Polygon and store it. We'll loop through all these to draw them on each step()
-    }
-  })();
-
-  var source = createImageOfNonPolygonalShapes(); // create an image of the remaining non-polygonal shapes
-
-  function step(timestamp) { // for each step through the animation
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
-    ctx.globalCompositeOperation = compmode.value; // update the "blend mode"
-
-    for(var i = 0; i < polygonData.length; i++) { // for each polygon
-      var polygon = polygonData[i];
-
-      drawShape(ctx,polygon); // draw the shape onto the <canvas>
-
-      polygon.step();
-    }
-
-    if(drawNonPoly.checked) ctx.drawImage(source, (window.innerWidth / 2 )- (522/2), (window.innerHeight / 2 )- (620/2)); // draw non-polygonal shapes and center them on <canvas>
-
-    window.requestAnimationFrame(step); // keep the animation running
-  }
-
-  window.requestAnimationFrame(step); // start the animation
-
-  function drawShape(ctx,polygon) { // draw the polygon data to a <canvas> context
-    var points = polygon.points.split(' '),
-    strokeStyle = polygon.fill,
-    fillStyle = polygon.fill;
-
-    if(followMouseCheckbox.checked) { // "follow the mouse" if we should
-      points[0] = (lastMousePos.x) + ',' + (lastMousePos.y);
-    }
-
-    ctx.strokeStyle = strokeStyle;
-    ctx.fillStyle = fillStyle;
-
-    ctx.beginPath();
-
-    for(var i = 0; i < points.length; i++) { // for each point in the polygon
-      var point = points[i],
-      x = parseFloat(point.split(',')[0]),
-      y = parseFloat(point.split(',')[1]);
-
-      if(oscillate.checked) { // apply oscillation effect using a sin wave
-        if(i == 1) {
-          x += Math.sin(polygon.tick) * oscillationRange.value; // second point in polygon moves horizontally
-        } else if(i == 2) {
-          y += Math.sin(polygon.tick) * oscillationRange.value; // third point in polygol moves vertically
-        }
-      }
-
-      if(i !== 0 || !followMouseCheckbox.checked) { // adjust coordinates for stage size
-        x += (window.innerWidth - 522) / 2;
-        y += (window.innerHeight - 620) / 2;
-      }
-
-      if(i<1) {
-        ctx.moveTo(x,y); // move to the position of the first point
-      } else {
-        ctx.lineTo(x,y); // draw lines to the rest
-      }
-    }
-
-    ctx.closePath(); // close the path and set the fill style
-
-    if(fillPolygons.checked) ctx.fill(); // fill the path or
-    else ctx.stroke(); // stroke the path
-  }
-
-  function createImageOfNonPolygonalShapes() { // takes the remaining <svg> data from our #wolf element and prepare it for being drawn to the canvas
-    var source = new Image();
-    var DOMURL = window.URL || window.webkitURL || window;
-
-    var data = document.querySelector("#wolf").outerHTML;
-
-    var svgData = new XMLSerializer().serializeToString(document.querySelector("#wolf"));
-    var blob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
-
-    var domURL = self.URL || self.webkitURL || self;
-    var url = domURL.createObjectURL(blob);
-
-    source.width = '522';
-    source.height = '620';
-
-    source.addEventListener('load', function () {
-      ctx.drawImage(source, (window.innerWidth / 2 )- (522/2), (window.innerHeight / 2 )- (620/2));
-      domURL.revokeObjectURL(url);
-    });
-
-    source.src = url;
-
-    return source;
-  }
-
-  function checkUrlParams() { 
-    var elements = document.querySelectorAll('[data-url-param]');
-    for(var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      var key = element.getAttribute('data-url-param');
-      if($_GET(key) !== null) {
-        if(element.type == "checkbox") {
-          element.checked = ($_GET(key) == "1") ? true : false;
-        } else {
-          element.value = $_GET(key);
-        }
-      }
-    }
-  }
-}
-
-function $_GET(param) { // http://www.creativejuiz.fr/blog/en/javascript-en/read-url-get-parameters-with-javascript
-	var vars = {};
-	window.location.href.replace(
-		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
-		function( m, key, value ) { // callback
-			vars[key] = value !== undefined ? value : '';
-		}
-	);
-
-	if ( param ) {
-		return vars[param] ? vars[param] : null;
-	}
-	return vars;
 }
